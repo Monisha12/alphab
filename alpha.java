9a4b4ebe6691ad53ec314bdc4a3ef6cfbfe8122b
Monisha12 <monichandran12@gmail.com>
2017-06-14 17:03:52 +0530

    alpha.java

---
+++ alpha.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class alpha
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char ip=s.next().charAt(0);
+if(ip=='a' || ip=='e' ||ip=='i'||ip=='o'||ip=='u')
+{
+System.out.println("character is alphabet");
+}
+else 
+{
+System.out.println("character is consonant");
+}
+}
+}
